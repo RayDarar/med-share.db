d615b3893739f317afefa3abe2a8c6263157343c
RayDarar <dfqgth400@gmail.com>
2020-12-03 20:02:51 +0600

    work: medicines page

--- src/queries/functions/similarity.sql
+++ src/queries/functions/similarity.sql
@@ -1,5 +1,8 @@
 create or replace function similarity(string varchar2, substring varchar2)
 return number is
 begin
-  return (length(string) - UTL_MATCH.EDIT_DISTANCE(substring, string)) / length(substring);
+--  return (length(string) - UTL_MATCH.EDIT_DISTANCE(substring, string)) / length(substring);
+  return UTL_MATCH.JARO_WINKLER_SIMILARITY(string, substring);
 end;
+
+-- Europharma
--- src/queries/packages/medicines.sql
+++ src/queries/packages/medicines.sql
@@ -3,6 +3,16 @@ is
   procedure get_autocomplete(
     p_query varchar2,
     p_result out sys_refcursor);
+  procedure get_medicines(
+    p_query varchar2,
+    p_result out sys_refcursor,
+    p_offset integer default 0);
+  procedure get_medicine(
+    p_id integer,
+    p_result out sys_refcursor);
+  procedure get_analogs(
+    p_id integer,
+    p_result out sys_refcursor);
 end;
 
 create or replace package body medicines
@@ -13,6 +23,44 @@ is
   begin
     open p_result for
     select id, title, similarity(title, p_query) as score from drugs
-    order by score desc;
+    order by score desc
+    FETCH NEXT 10 ROWS ONLY;
+  end;
+  procedure get_medicines(
+    p_query varchar2,
+    p_result out sys_refcursor,
+    p_offset integer default 0) is
+  begin
+    open p_result for
+    select id, title, status, similarity(title, p_query) as score from drugs
+    order by score desc
+    OFFSET p_offset ROWS
+    FETCH NEXT 10 ROWS ONLY;
+  end;
+  procedure get_medicine(
+    p_id integer,
+    p_result out sys_refcursor) is
+  begin
+    open p_result for
+    select * from drugs
+    where id = p_id;
+  end;
+  procedure get_analogs(
+    p_id integer,
+    p_result out sys_refcursor) is
+  begin
+    open p_result for
+    select d.id, d.title, d.status, a.analog_id 
+    from drugs d join drugs_analogs a on (d.id = a.analog_id)
+    where a.drug_id = p_id;
   end;
 end;
+
+
+
+
+
+
+
+
+
